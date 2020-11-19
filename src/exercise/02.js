edc1507d05d15e06d76f54673ce9e51621fb8e52
Ondrej Bajcar <ondrej@Ondrejs-MacBook-Pro.local>
2020-11-19 15:19:55 +0100

    complete the module

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,23 +3,30 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+const useLocalStorageWithState = (key, initialState = '') => {
+  const [state, setState] = React.useState(() => {
+    const value = window.localStorage.getItem(key)
+    return value ? JSON.parse(value) : initialState
+  })
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageWithState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +34,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="asf" />
 }
 
 export default App
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,10 +6,17 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {ErrorBoundary} from 'react-error-boundary'
+import {
+  PokemonForm,
+  PokemonInfoFallback,
+  PokemonDataView,
+  fetchPokemon,
+} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [state, setState] = React.useState({status: 'idle'})
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
@@ -19,13 +26,73 @@ function PokemonInfo({pokemonName}) {
   //   fetchPokemon('Pikachu').then(
   //     pokemonData => { /* update all the state here */},
   //   )
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+    setState({status: 'pending'})
+    fetchPokemon(pokemonName).then(
+      pokemon => {
+        setState({status: 'resolved', pokemon})
+      },
+      error => {
+        setState({status: 'rejected', error})
+      },
+    )
+  }, [pokemonName])
+
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const {status, pokemon, error} = state
+  switch (status) {
+    case 'rejected':
+      throw error
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />
+    case 'resolved':
+      return <PokemonDataView pokemon={pokemon} />
+    default:
+      return 'Submit a pokemon'
+  }
+}
 
-  // 💣 remove this
-  return 'TODO'
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {error: null}
+//   }
+
+//   static getDerivedStateFromError(error) {
+//     // Update state so the next render will show the fallback UI.
+//     return {error}
+//   }
+
+//   render() {
+//     const {error} = this.state
+//     if (error) {
+//       // You can render any custom fallback UI
+//       return (
+//         <div role="alert">
+//           There was an error:{' '}
+//           <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+//         </div>
+//       )
+//     }
+
+//     return this.props.children
+//   }
+// }
+
+function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
 }
 
 function App() {
@@ -40,10 +107,41 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
+        >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
 }
 
 export default App
+
+// When you make a function async, it automatically returns a promise
+// (whether you’re not returning anything at all, or explicitly returning a function).
+// This is due to the semantics of async/await syntax.
+// So if you want to use async/await in useEffect, the best way to do that is like so:
+// React.useEffect(() => {
+//   async function effect() {
+//     const result = await doSomeAsyncThing()
+//     // do something with the result
+//   }
+//   effect()
+// })
+
+// alternatively:
+// React.useEffect(() => {
+//   doSomeAsyncThing().then(result => {
+//     // do something with the result
+//   })
+// })
+
+// You’ll notice that we’re calling a bunch of state updaters in a row.
+// This is normally not a problem, but each call to our state updater can result
+// in a re-render of our component. React normally batches these calls so you
+// only get a single re-render, but it’s unable to do this in an asynchronous callback
+// (like our promise success and error handlers).
